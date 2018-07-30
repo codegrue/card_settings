293f05eb5ed1c84c16b5e4288693811013d0e304
Jeffrey Jorczak <jeff@Jorczak-MacBook-Pro.local>
2018-07-30 16:04:05 -0400

    renamed

--- lib/card_settings.dart
+++ lib/card_settings.dart
@@ -4,21 +4,21 @@
 /// Package for building card based settings forms
 library card_settings;
 
-export 'controls/card_settings_panel.dart';
-export 'controls/card_settings_field.dart';
-export 'controls/card_settings_header.dart';
-export 'controls/card_settings_text.dart';
-export 'controls/card_settings_double.dart';
-export 'controls/card_settings_int.dart';
-export 'controls/card_settings_paragraph.dart';
-export 'controls/card_settings_switch.dart';
-export 'controls/card_settings_list_picker.dart';
-export 'controls/card_settings_number_picker.dart';
-export 'controls/card_settings_color_picker.dart';
-export 'controls/card_settings_date_picker.dart';
-export 'controls/card_settings_time_picker.dart';
-export 'controls/card_settings_button.dart';
-export 'controls/card_settings_instructions.dart';
+export 'widgets/card_settings_panel.dart';
+export 'widgets/card_settings_field.dart';
+export 'widgets/card_settings_header.dart';
+export 'widgets/card_settings_text.dart';
+export 'widgets/card_settings_double.dart';
+export 'widgets/card_settings_int.dart';
+export 'widgets/card_settings_paragraph.dart';
+export 'widgets/card_settings_switch.dart';
+export 'widgets/card_settings_list_picker.dart';
+export 'widgets/card_settings_number_picker.dart';
+export 'widgets/card_settings_color_picker.dart';
+export 'widgets/card_settings_date_picker.dart';
+export 'widgets/card_settings_time_picker.dart';
+export 'widgets/card_settings_button.dart';
+export 'widgets/card_settings_instructions.dart';
 
 export 'helpers/picker_dialog.dart';
 export 'helpers/scroll_picker.dart';
