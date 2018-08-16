4947930f43c52ca2948abb19390abfbfb90e7ab7
Jeffrey Jorczak <jeff@Jorczak-MacBook-Pro.local>
2018-08-15 21:56:39 -0400

    updated test reference

--- test/converters_test.dart
+++ test/converters_test.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:card_settings/card_settings.dart';
 
