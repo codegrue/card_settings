b26c57479ce2d04118647179dc32f74f86608854
Jeffrey Jorczak <jeff@Jorczak-MacBook-Pro.local>
2020-05-23 19:48:21 -0400

    added tests for CardSettingsInstructions

---
+++ test/card_settings_header_test.dart
@@ -0,0 +1,35 @@
+import 'package:card_settings/card_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CardSettingsHeader', () {
+    Widget widgetTree;
+    var label = "ArbitraryTitle";
+
+    setUpAll(() async {
+      widgetTree = MaterialApp(
+        home: CardSettings(
+          children: [
+            CardSettingsSection(
+              children: [
+                CardSettingsHeader(
+                  label: label,
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    });
+
+    testWidgets('displays properties', (WidgetTester tester) async {
+      // arrange
+      await tester.pumpWidget(widgetTree);
+
+      // assert
+      final labelFinder = find.text(label);
+      expect(labelFinder, findsOneWidget);
+    });
+  });
+}
---
+++ test/card_settings_instructions_test.dart
@@ -0,0 +1,39 @@
+import 'package:card_settings/card_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CardSettingsInstructions', () {
+    Widget widgetTree;
+    var instructions = "Don't panic!";
+    var color = Colors.indigo;
+
+    setUpAll(() async {
+      widgetTree = MaterialApp(
+        home: CardSettings(
+          children: [
+            CardSettingsSection(
+              children: [
+                CardSettingsInstructions(
+                  text: instructions,
+                  textColor: color,
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    });
+
+    testWidgets('displays properties', (WidgetTester tester) async {
+      // arrange
+      await tester.pumpWidget(widgetTree);
+
+      // assert
+      final instructionsFinder = find.text(instructions);
+      expect(instructionsFinder, findsOneWidget);
+      final value = instructionsFinder.evaluate().first.widget as Text;
+      expect(value.style.color, color);
+    });
+  });
+}
